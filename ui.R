3c7e0e319aa891efb14ed1122bd1725dac5cef72
Boxuan Cui <boxuancui@gmail.com>
2015-02-10 12:25:18 -0500

    enabled tracking

--- ui.R
+++ ui.R
@@ -5,6 +5,7 @@ library(threejs)
 shinyUI(
   fluidPage(
     titlePanel("Top 10000 Populated Cities"),
+    tags$head(includeScript("google_analytics.js")),
     tags$style("body {background-color: black;} h2 {color: white;}"),
     theme=shinytheme("cosmo"),
     fluidRow(
