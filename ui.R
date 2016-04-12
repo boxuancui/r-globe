fb8f28d9fb932947b3bdf9a30410d7c591e83d40
Boxuan Cui <bcui@smartertravel.com>
2016-04-12 15:07:44 -0400

    Removed contact author URL

--- ui.R
+++ ui.R
@@ -8,9 +8,6 @@ shinyUI(
     tags$head(includeScript("google_analytics.js")),
     tags$style("body {background-color: black;} h2 {color: white;}"),
     theme=shinytheme("cosmo"),
-    fluidRow(
-      column(2, tags$a("Contact Author", href="mailto:cui.bo@bcg.com?Subject=r-globe app"))
-    ),
     fluidRow(globeOutput("globe"))
   )
 )
